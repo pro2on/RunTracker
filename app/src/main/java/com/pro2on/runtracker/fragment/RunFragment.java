8952c603004e68af81b042026ef32f46d9928cb6
pro2on <k.chupryna@gmail.com>
2015-05-06 11:47:09 +0300

    implement ToolBar as as supportActionBar

--- app/src/main/java/com/pro2on/runtracker/fragment/RunFragment.java
+++ app/src/main/java/com/pro2on/runtracker/fragment/RunFragment.java
@@ -3,6 +3,8 @@ package com.pro2on.runtracker.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,21 @@ import com.pro2on.runtracker.R;
  */
 public class RunFragment extends Fragment {
 
+
+    private Toolbar mToolBar;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_run, container, false);
+
+        mToolBar = (Toolbar) view.findViewById(R.id.my_awesome_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolBar);
+        mToolBar.setNavigationIcon(R.mipmap.ic_maps_directions_walk);
+
+
+
         return view;
     }
 }
