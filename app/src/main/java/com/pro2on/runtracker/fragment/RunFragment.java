57e838dbca6fc269db2d587e9192cd9d945d354d
pro2on <k.chupryna@gmail.com>
2015-05-06 11:07:07 +0300

    add RunFragment layout

--- app/src/main/java/com/pro2on/runtracker/fragment/RunFragment.java
+++ app/src/main/java/com/pro2on/runtracker/fragment/RunFragment.java
@@ -1,9 +1,23 @@
 package com.pro2on.runtracker.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pro2on.runtracker.R;
 
 /**
  *  Created by pro2on on 06.05.15.
  */
 public class RunFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_run, container, false);
+        return view;
+    }
 }
